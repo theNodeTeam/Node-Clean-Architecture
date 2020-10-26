145a7ad5c4c2072fefe8e43032b8f28860ef3e15
Yasir Memood <57556222+yasirmehmood34500@users.noreply.github.com>
2020-10-26 10:32:56 -0700

    Update connection.js

--- db/mysql/connection.js
+++ db/mysql/connection.js
@@ -1,25 +1,31 @@
 const mysql = require('mysql')
 
 
-const db = mysql.createConnection({
-    // host: 'localhost',
-    // user: 'root',
-    // password: '',
-    // database: 'node_grocery2'
-    host: 'us-cdbr-east-02.cleardb.com',
-    user: 'bb5e40a33386b1',
-    password: '8981812c',
-    database: 'heroku_2e9b6dd752ebf23',
-    port: 3306
-})
+// const db = mysql.createConnection({
+//     // host: 'localhost',
+//     // user: 'root',
+//     // password: '',
+//     // database: 'node_grocery2'
+//     host: 'us-cdbr-east-02.cleardb.com',
+//     user: 'bb5e40a33386b1',
+//     password: '8981812c',
+//     database: 'heroku_2e9b6dd752ebf23',
+//     port: 3306
+// })
 
-
-db.connect((err => {
-    if (err) {
-        throw err
-    }
-    console.log("mysql database connected")
-}))
+// db.connect((err => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log("mysql database connected")
+// }))
+var db  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'us-cdbr-east-02.cleardb.com',
+    user            : 'bb5e40a33386b1',
+    password        : '8981812c',
+    database        : 'heroku_2e9b6dd752ebf23'
+  });
 db.Promise = global.Promise;
 global.db = db
 
