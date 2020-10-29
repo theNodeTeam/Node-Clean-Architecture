2d21368187a1420c561d01baa502f5bf094ec1db
Yasir Memood <57556222+yasirmehmood34500@users.noreply.github.com>
2020-10-29 05:06:54 -0700

    Update index.js

--- drivers/webserver/routes/index.js
+++ drivers/webserver/routes/index.js
@@ -5,9 +5,9 @@ const products = require('./products')
 
 //products
 router
-  .get('/v1/products', products.index) // mysq done
-  .get('/v1/products/:id', products.show)  // mysql done
-  .post('/v1/products', products.create) // mysql done 
+  .get('/products', products.index) // mysq done
+  .get('/products/:id', products.show)  // mysql done
+  .post('/products', products.create) // mysql done 
   .put("/v1/product/:id", products.editProduct) // mysql done
 
 //category
@@ -27,8 +27,8 @@ router
 //item
 router
   .get("/v1/item", products.getItems) // mysql done
-  .post("/v1/item", products.addItem) // mysql done (specialInstrauctions is not defined)
-  .put("/v1/item/:id", products.editItem) // mysql done (specialInstrauctions is not defined)
+  .post("/v1/item", products.addItem) // mysql done 
+  .put("/v1/item/:id", products.editItem) // mysql done 
   .get("/v1/item/:id", products.getItem) // mysql done
   .delete("/v1/item/:id", products.deleteItem) // mysql done
   .get("/v1/item/store/:id", products.getStoreItem) // mysql done
@@ -41,20 +41,20 @@ router
   .post("/v1/ref_prod_fav", products.addRef_prod_fav) // mysql done 
   .put("/v1/ref_prod_fav/:id", products.editRef_prod_fav) // mysql done
   .delete("/v1/ref_prod_fav/:id", products.deleteRef_prod_fav) // mysql done
-  .get("/v1/user/ref_prod_fav/:id", products.userRef_prod_fav) // debug the logic // we need items and products od the user by userID
+  .get("/v1/user/ref_prod_fav/:id", products.userRef_prod_fav) // mysql done
   .get("/v1/store/user/ref_prod_fav/:id/:storeID", products.userStoreRef_prod_fav) // mysql done
 
 //order-product
 router
-  .get("/v1/ref_trans_products/:id", products.getRef_trans_prod) // mysql done
-  .post("/v1/ref_trans_products", products.addRef_trans_products) // mysql done 
-  .put("/v1/ref_trans_products/:orderID/:itemID", products.editRef_trans_prod) // new url
-  .delete("/v1/ref_trans_products/:orderID/:itemID", products.deleteRef_trans_prod) // new url
+  .get("/v1/ref_trans_products/:orderID", products.getRef_trans_prod) // mysql done
+  .post("/v1/ref_trans_products", products.addRef_trans_products) // mysql done  (transProdItem.itemQuantity is not a function)
+  .put("/v1/ref_trans_products/:orderID/:itemID", products.editRef_trans_prod) // mysql done
+  .delete("/v1/ref_trans_products/:orderID/:itemID", products.deleteRef_trans_prod) // mysql done
 
 //nutritions
-  .get("/v1/nutrition/:id", products.get_nutrition)
-  .post("/v1/nutrition", products.add_nutrition)
-  .put("/v1/nutrition/:id", products.edit_nutrition) 
-  .delete("/v1/nutrition/:id", products.delete_nutrition) 
+  .get("/v1/nutrition/:id", products.get_nutrition) //  mysql done
+  .post("/v1/nutrition", products.add_nutrition) //  mysql done
+  .put("/v1/nutrition/:id", products.edit_nutrition) //  mysql done
+  .delete("/v1/nutrition/:id", products.delete_nutrition) //  mysql done
 
 module.exports = router  
