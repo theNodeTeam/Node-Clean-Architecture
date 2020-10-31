a134b781630937b370847a4420f2a28a070203f4
Aijazalishah <aijazalishah333@gmail.com>
2020-10-31 01:39:59 +0000

    SD

--- drivers/webserver/routes/index.js
+++ drivers/webserver/routes/index.js
@@ -41,7 +41,7 @@ router
   .post("/v1/ref_prod_fav", products.addRef_prod_fav) // mysql done 
   .put("/v1/ref_prod_fav/:id", products.editRef_prod_fav) // mysql done
   .delete("/v1/ref_prod_fav/:id", products.deleteRef_prod_fav) // mysql done
-  .get("/v1/user/ref_prod_fav/:id", products.userRef_prod_fav) // debug the logic // we need items and products od the user by userID
+  .get("/v1/user/ref_prod_fav/:id", products.userRef_prod_fav) // mysql Done
   .get("/v1/store/user/ref_prod_fav/:id/:storeID", products.userStoreRef_prod_fav) // mysql done
 
 //order-product
@@ -49,7 +49,7 @@ router
   .get("/v1/ref_trans_products/:id", products.getRef_trans_prod) // mysql done
   .post("/v1/ref_trans_products", products.addRef_trans_products) // mysql done 
   .put("/v1/ref_trans_products/:orderID/:itemID", products.editRef_trans_prod) // new url
-  .delete("/v1/ref_trans_products/:orderID/:itemID", products.deleteRef_trans_prod) // new url
+  .delete("/v1/ref_trans_products/:orderID/:itemID", products.deleteRef_trans_prod) // new url // send remianing items of order by orderID
 
 //nutritions
   .get("/v1/nutrition/:id", products.get_nutrition)
