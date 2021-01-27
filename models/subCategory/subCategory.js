6c48b9c36a512d3baabb990a908d9103a4bee610
Aijazalishah <aijazalishah@live.com>
2021-01-27 08:12:32 +0000

    changes

--- models/subCategory/subCategory.js
+++ models/subCategory/subCategory.js
@@ -11,14 +11,14 @@ let buildMakeSubCategory = function(subCategoryValidator) {
     subCategoryName,
     subCategoryDescription,
     categoryID,
-    subCategoryActive
+    // subCategoryActive
     // productID
   } = {}) => {
     let {error} = subCategoryValidator({
       subCategoryName, 
       subCategoryDescription, 
       categoryID,
-      subCategoryActive
+      // subCategoryActive
       // productID
     })
     if (error) throw new Error(error)
@@ -27,7 +27,7 @@ let buildMakeSubCategory = function(subCategoryValidator) {
       getSubCategoryName: () => subCategoryName,
       getSubCategoryDescription: () => subCategoryDescription,
       getCategoryID: () => categoryID,
-      subCategoryActive: () => subCategoryActive
+      // subCategoryActive: () => subCategoryActive
       // getProductID: () => productID
     }
   }
